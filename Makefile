af87051f98c75ca6e5e34b1a83f28c4164e6b08a
Roland Nádor <rolandnador@gmail.com>
2023-05-08 15:27:02 +0200

    library trials

---
+++ Makefile
@@ -0,0 +1,2 @@
+parser:	./src/main.c
+	gcc -o ./parser ./src/main.c ./lib/libwebp/libwebp.a ./lib/libwebp/libwebpdemux.a ./lib/libwebp/libwebpmux.a
---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "../include/libwebp/encode.h""
+
+int main() {
+    printf("Hello World\n");
+    WebPGetEncoderVersion();
+    return 0;
+}
